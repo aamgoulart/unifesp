940068dcf89d4ce98b728ed0fe496c427af877ec
aamgoulart <aamgoulart@outlook.com>
2022-01-04 16:42:07 -0300

    .

--- REDES-2021-1/tcpClient.c
+++ REDES-2021-1/tcpClient.c
@@ -5,6 +5,7 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <netinet/in.h>
+#include<netdb.h>	//hostent
 #include <arpa/inet.h>
 
 #define ERROR_CONNECTION "Error on try connect on server\n"
@@ -14,16 +15,22 @@
 #define SUCESS_DISCONECT "Bye\n"
 #define ERROR_DATA "Error on recieve data\n"
 
+int hostname_to_ip(char * hostname , char* ip);
 
 int main(int argc, char **argv){
 
 	int clientSocket, ret;
 	struct sockaddr_in serverAddr;
 	char buffer[1024], port, address;
+	char ip[100];
+
 
     if(argc != 3) { 
         printf("Use: %s <porta> <endereço server>\n", argv[0]);
     }
+
+	hostname_to_ip(argv[2] , ip);
+	printf("%s resolved to %s" , argv[2] , ip);
 
     //SOCKET
 
@@ -40,7 +47,7 @@ int main(int argc, char **argv){
 	memset(&serverAddr, '\0', sizeof(serverAddr));
 	serverAddr.sin_family = AF_INET;
 	serverAddr.sin_port = htons(atoi(argv[1]));
-	serverAddr.sin_addr.s_addr = inet_addr(argv[2]);
+	serverAddr.sin_addr.s_addr = inet_addr(ip);
 
 	ret = connect(clientSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr));
 	if(ret < 0){
@@ -73,4 +80,29 @@ int main(int argc, char **argv){
     close(clientSocket);
 
 	return 0;
+}
+
+int hostname_to_ip(char * hostname , char* ip)
+{
+	struct hostent *he;
+	struct in_addr **addr_list;
+	int i;
+		
+	if ( (he = gethostbyname( hostname ) ) == NULL) 
+	{
+		// get the host info
+		herror("gethostbyname");
+		return 1;
+	}
+
+	addr_list = (struct in_addr **) he->h_addr_list;
+	
+	for(i = 0; addr_list[i] != NULL; i++) 
+	{
+		//Return the first one;
+		strcpy(ip , inet_ntoa(*addr_list[i]) );
+		return 0;
+	}
+	
+	return 1;
 }
--- REDES-2021-1/tcpServer.c
+++ REDES-2021-1/tcpServer.c
@@ -6,6 +6,7 @@
 #include <sys/types.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include<netdb.h>	//hostent
 
 #define ERROR_CONNECTION "Error on try connect on client\n"
 #define SUCCESS_CONNETION "Conect on client\n"
@@ -16,6 +17,8 @@
 #define ERROR_BIND "Error on bind\n"
 #define SUCESS_BIND "Sucess bind\n"
 #define WAITING_CLIENTE "Waiting response for client\n"
+
+int hostname_to_ip(char * hostname , char* ip);
 
 int main(int argc, char **argv){
 
@@ -30,9 +33,14 @@ int main(int argc, char **argv){
 	char buffer[1024];
 	pid_t childpid;
 
+	char ip[100];
+
     if(argc != 3) { 
         printf("Use: %s <porta> <endereço server>\n", argv[0]);
     }
+
+	hostname_to_ip(argv[2] , ip);
+	printf("%s resolved to %s" , argv[2] , ip);
 
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
 	if(sockfd < 0){
@@ -44,7 +52,7 @@ int main(int argc, char **argv){
 	memset(&serverAddr, '\0', sizeof(serverAddr));
 	serverAddr.sin_family = AF_INET;
 	serverAddr.sin_port = htons(atoi(argv[1]));
-	serverAddr.sin_addr.s_addr = inet_addr(argv[2]);
+	serverAddr.sin_addr.s_addr = inet_addr(ip);
 
 	ret = bind(sockfd, (struct sockaddr*)&serverAddr, sizeof(serverAddr));
 	if(ret < 0){
@@ -86,4 +94,29 @@ int main(int argc, char **argv){
 
 
 	return 0;
+}
+
+int hostname_to_ip(char * hostname , char* ip)
+{
+	struct hostent *he;
+	struct in_addr **addr_list;
+	int i;
+		
+	if ( (he = gethostbyname( hostname ) ) == NULL) 
+	{
+		// get the host info
+		herror("gethostbyname");
+		return 1;
+	}
+
+	addr_list = (struct in_addr **) he->h_addr_list;
+	
+	for(i = 0; addr_list[i] != NULL; i++) 
+	{
+		//Return the first one;
+		strcpy(ip , inet_ntoa(*addr_list[i]) );
+		return 0;
+	}
+	
+	return 1;
 }
